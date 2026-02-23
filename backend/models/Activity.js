8256779c57d0d25c497bb4d0a1e3ea6bb03505bb
Jabez <jabezryan11@gmail.com>
2026-02-23 15:15:27 +1300

    fixed title

--- backend/models/Activity.js
+++ backend/models/Activity.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 
 const activitySchema = new mongoose.Schema(
-{
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    content: { type: String, required: true}
-},
-{ timestamps: true},
+  },
+  { timestamps: true }
 );
 
 export default mongoose.model("Activity", activitySchema);
